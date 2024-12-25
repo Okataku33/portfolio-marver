8c53dedfeda151c0735dbdf18a12056db3afd8df
okataku <tyanoka3433@gmail.com>
2024-12-25 13:37:50 +0900

    ヘッダーからお気に入り画面への遷移できるように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -75,7 +75,7 @@ export default function Header() {
   // メニュー項目の定義
   const menuItems = [
     { name: "HOME", href: "/" },
-    { name: "Q&A", href: "/qanda" },
+    { name: "Q&A", href: "qanda" },
     ...(isAuth
       ? [{ name: "ログアウト", href: "#", onClick: handleLogout }]
       : [{ name: "ログイン", href: "#", onClick: loginInWithGoogle }]),
